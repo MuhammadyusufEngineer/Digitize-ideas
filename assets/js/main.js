e6dec3c8aff4e56935fe20056aa7f393b1fdf58d
Muhammadyusuf Engineer <mrimofficial7@gmail.com>
2023-11-06 12:54:09 +0500

    all fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,8 @@
-// Preload images before showing content
-const preloadImages = () => {
-    return new Promise((resolve, reject) => {
-        imagesLoaded(document.querySelectorAll('img'), resolve);
-    });
-};
-// And then..
-preloadImages().then(() => {
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { imagesLoaded } from "imagesloaded"
+gsap.registerPlugin(ScrollTrigger);
+function startLoading() {
     // Remove the loader
     startLoader()
     gsap.to('.counter', 0.25, {
@@ -29,7 +26,7 @@ preloadImages().then(() => {
         className: "hidden",
         overwrite: "flex"
     })
-});
+};
 
 function startLoader() {
     gsap.set('body', { overflowY: 'hidden' })
@@ -59,9 +56,6 @@ function toggleScrolling(condition) {
     body.style.overflowY = condition ? "scroll" : 'hidden'
 }
 
-function registerScrollTrigger() {
-    gsap.registerPlugin(ScrollTrigger)
-}
 
 let menuBtn = document.querySelector('.menu-btn')
 let menuList = document.querySelector('.menu-list')
@@ -295,7 +289,6 @@ function mouseEntrToEl(html = '', num1, num2, num3, num4) {
     p.innerHTML = `${html}`
     p.classList.replace(`scale-${num3}`, `scale-${num4}`)
 
-    console.log(html, num1, num2, num3, num4)
 }
 
 // #--------------------------------------------------------------#
@@ -420,230 +413,232 @@ footer3D.style.textShadow = shadow
 // });
 
 window.addEventListener('DOMContentLoaded', () => {
-    registerScrollTrigger()
+    startLoading()
     loadingAnimation()
 
+});
 
-    const groups = gsap.utils.toArray(".accordion-item");
-    const menus = gsap.utils.toArray(".accordion-title");
 
-    const animations = [];
+const groups = gsap.utils.toArray(".accordion-item");
+const menus = gsap.utils.toArray(".accordion-title");
 
-    groups.forEach(group => createAnimation(group));
+const animations = [];
 
-    menus.forEach(menu => {
-        menu.addEventListener("click", () => toggleAnimation(menu));
+groups.forEach(group => createAnimation(group));
+
+menus.forEach(menu => {
+    menu.addEventListener("click", () => toggleAnimation(menu));
+});
+
+function toggleAnimation(menu) {
+    // Save the current state of the clicked animation
+    const selectedReversedState = menu.animation.reversed();
+
+    // Reverse all animations
+    animations.forEach(animation => animation.reverse());
+
+    // Set the reversed state of the clicked accordion element to the opposite of what it was before
+    menu.animation.reversed(!selectedReversedState);
+}
+
+function createAnimation(element) {
+    const menu = element.querySelector(".accordion-title");
+    const box = element.querySelector(".accordion-content");
+    const icon = element.querySelector(".accordion-icon");
+
+    gsap.set(box, { height: "auto" })
+    gsap.set(icon, { translate: 'rotate(45deg)' })
+    // groups.forEach(item => gsap.set(item, { className: "active" }))
+
+    // Keep a reference to the animation on the menu item itself
+    const tween = gsap.from(box, {
+        height: 0,
+        margin: 0,
+        duration: 0.5,
+        ease: "power2.inOut",
+        reversed: true,
     });
 
-    function toggleAnimation(menu) {
-        // Save the current state of the clicked animation
-        const selectedReversedState = menu.animation.reversed();
 
-        // Reverse all animations
-        animations.forEach(animation => animation.reverse());
+    menu.animation = tween;
+    animations.push(tween);
+}
+// class HoverImage {
+//     constructor(element, options) {
+//         this.el = element;
+//         this.imgUrl = element.dataset.hoverImg;
+//         this.img = this.createHoverImage();
+//         this.imgDimensions = this.getDimensions(this.img);
+//         this.init();
 
-        // Set the reversed state of the clicked accordion element to the opposite of what it was before
-        menu.animation.reversed(!selectedReversedState);
-    }
+//         this.options = Object.assign({}, {
+//             maxVel: 20,
+//             lerp: 0.1,
+//             base: 0.085,
+//             delta: 0.0005,
+//         }, options);
 
-    function createAnimation(element) {
-        const menu = element.querySelector(".accordion-title");
-        const box = element.querySelector(".accordion-content");
-        const icon = element.querySelector(".accordion-icon");
+//         this.x = 0;
+//         this.y = 0;
+//         this.lastX = 0;
+//         this.lastY = 0;
+//         this.vel = { x: 0, y: 0 };
+//         this.lerpVel = { x: 0, y: 0 };
+//         this.paused = false;
+//         this.points = this.getPoints();
+//         this.maskPath = this.getMaskPath();
+//         this.mask = this.createMask();
+//         this.render();
 
-        gsap.set(box, { height: "auto" })
-        gsap.set(icon, { translate: 'rotate(45deg)' })
-        // groups.forEach(item => gsap.set(item, { className: "active" }))
+//     }
+//     render() {
+//         if (this.paused === true) return;
+//         requestAnimationFrame(() => this.render());
+//         this.vel = {
+//             x: 100 / this.options.maxVel * clamp(this.x - this.lastX, -this.options.maxVel, this.options.maxVel),
+//             y: 100 / this.options.maxVel * clamp(this.y - this.lastY, -this.options.maxVel, this.options.maxVel),
+//         };
+//         this.lerpVel = {
+//             x: lerp(this.lerpVel.x, this.vel.x, this.options.lerp),
+//             y: lerp(this.lerpVel.y, this.vel.y, this.options.lerp),
+//         }
 
-        // Keep a reference to the animation on the menu item itself
-        const tween = gsap.from(box, {
-            height: 0,
-            margin: 0,
-            duration: 0.5,
-            ease: "power2.inOut",
-            reversed: true,
-        });
+//         this.points = this.getPoints();
 
+//         this.maskPath = this.getMaskPath();
+//         gsap.to(this.mask, {
+//             attr: { d: this.maskPath }
+//         });
 
-        menu.animation = tween;
-        animations.push(tween);
-    }
-    // class HoverImage {
-    //     constructor(element, options) {
-    //         this.el = element;
-    //         this.imgUrl = element.dataset.hoverImg;
-    //         this.img = this.createHoverImage();
-    //         this.imgDimensions = this.getDimensions(this.img);
-    //         this.init();
+//         const distance = Math.sqrt(Math.pow(this.vel.x, 2) + Math.pow(this.vel.y, 2));
+//         const scale = Math.min(distance * this.options.delta, 1);
+//         const angle = (this.vel.x * this.options.delta * 180) / Math.PI;
+//         gsap.to(this.img, {
+//             scale: 1 - scale,
+//             rotate: angle,
+//         })
 
-    //         this.options = Object.assign({}, {
-    //             maxVel: 20,
-    //             lerp: 0.1,
-    //             base: 0.085,
-    //             delta: 0.0005,
-    //         }, options);
+//         this.lastX = this.x;
+//         this.lastY = this.y;
+//     }
+//     init() {
+//         this.el.parentElement.addEventListener('mousemove', (e) => {
+//             if (this.paused === true) return;
+//             this.x = e.clientX;
+//             this.y = e.clientY;
+//             this.move();
+//         });
+//         this.el.addEventListener('mouseenter', () => {
+//             if (this.paused === true) return;
+//             this.toggleVisibility(this.img, true)
+//         });
+//         this.el.addEventListener('mouseleave', () => {
+//             if (this.paused === true) return;
+//             this.toggleVisibility(this.img, false)
+//         });
+//     }
+//     createHoverImage() {
+//         let imageElm = new Image(900);
+//         imageElm.src = this.imgUrl;
+//         imageElm.classList.add('hover-image');
+//         // let the browser rasterize the image and hide it after
+//         // cause strange behavior where browser dont really load images with opacity set to 0
+//         imageElm.addEventListener('load', () => {
+//             imageElm.style.opacity = '0.01';
+//             this.el.appendChild(imageElm);
+//             setTimeout(() => {
+//                 this.toggleVisibility(imageElm, false, 0);
+//             }, 100);
+//         });
+//         return imageElm;
+//     }
+//     move() {
+//         this.imgDimensions = this.getDimensions(this.img)
+//         const y = this.y - this.imgDimensions.height / 2;
+//         const x = this.x - this.imgDimensions.width / 2;
+//         gsap.to(this.img, {
+//             y: y,
+//             x: x,
+//         });
+//     }
+//     createMask() {
+//         let maskpath = document.querySelector('#hover-image__mask path');
+//         this.img.style.cssText +=
+//             '-webkit-clip-path: url(#hover-image__mask);clip-path: url(#hover-image__mask);';
+//         if (maskpath) return maskpath;
 
-    //         this.x = 0;
-    //         this.y = 0;
-    //         this.lastX = 0;
-    //         this.lastY = 0;
-    //         this.vel = { x: 0, y: 0 };
-    //         this.lerpVel = { x: 0, y: 0 };
-    //         this.paused = false;
-    //         this.points = this.getPoints();
-    //         this.maskPath = this.getMaskPath();
-    //         this.mask = this.createMask();
-    //         this.render();
+//         document.body.insertAdjacentHTML(
+//             'beforeend',
+//             `
+//                     <svg height="0" width="0" style="position:absolute;">
+//                         <!--   https://yqnn.github.io/svg-path-editor/ -->
+//                         <defs>
+//                             <clipPath id="hover-image__mask" clipPathUnits="objectBoundingBox">
+//                             <path
+//                                 d="${this.maskPath}"
+//                                 data-path-normal="${this.maskPath}"
+//                             />
+//                             </clipPath>
+//                         </defs>
+//                     </svg>
+//                     `
+//         );
+//         return document.querySelector('#hover-image__mask path');
+//     }
+//     toggleVisibility(el, show, duration = null) {
+//         let time = {};
+//         if (duration !== null) {
+//             time = {
+//                 duration: 0,
+//             };
+//         }
+//         gsap.to(el, {
+//             opacity: show ? 1 : 0,
+//             ...time,
+//         });
+//     }
+//     getMaskPath() {
+//         return `M ${this.options.base} ${this.options.base} C ${this.points.left.top} 0.25 ${this.points.left.bottom} 0.75 ${this.options.base} ${1 - this.options.base} C 0.25 ${this.points.bottom.left} 0.75 ${this.points.bottom.right} ${1 - this.options.base} ${1 - this.options.base} C ${this.points.right.bottom} 0.75 ${this.points.right.top} 0.25 ${1 - this.options.base} ${this.options.base} C 0.75 ${this.points.top.right} 0.25 ${this.points.top.left} ${this.options.base} ${this.options.base} Z`;
+//     }
+//     getPoints() {
+//         return {
+//             left: {
+//                 top: this.options.base + (this.options.base / 100 * this.lerpVel.x),
+//                 bottom: this.options.base + (this.options.base / 100 * this.lerpVel.x),
+//             },
+//             bottom: {
+//                 left: (1 - this.options.base) + (this.options.base / 100 * this.lerpVel.y),
+//                 right: (1 - this.options.base) + (this.options.base / 100 * this.lerpVel.y),
+//             },
+//             right: {
+//                 bottom: (1 - this.options.base) + (this.options.base / 100 * this.lerpVel.x),
+//                 top: (1 - this.options.base) + (this.options.base / 100 * this.lerpVel.x),
+//             },
+//             top: {
+//                 right: this.options.base + (this.options.base / 100 * this.lerpVel.y),
+//                 left: this.options.base + (this.options.base / 100 * this.lerpVel.y),
+//             },
+//         };
+//     }
+//     getDimensions(el) {
+//         return { width: el.clientWidth, height: el.clientHeight };
+//     }
+//     start() {
+//         this.paused = false;
+//         this.render();
+//     }
+//     stop() {
+//         this.paused = true;
+//     }
+// }
+// function clamp(val, min, max) {
+//     return Math.min(Math.max(val, min), max);
+// }
+// function lerp(v0, v1, t) {
+//     return v0 * (1 - t) + v1 * t;
+// }
 
-    //     }
-    //     render() {
-    //         if (this.paused === true) return;
-    //         requestAnimationFrame(() => this.render());
-    //         this.vel = {
-    //             x: 100 / this.options.maxVel * clamp(this.x - this.lastX, -this.options.maxVel, this.options.maxVel),
-    //             y: 100 / this.options.maxVel * clamp(this.y - this.lastY, -this.options.maxVel, this.options.maxVel),
-    //         };
-    //         this.lerpVel = {
-    //             x: lerp(this.lerpVel.x, this.vel.x, this.options.lerp),
-    //             y: lerp(this.lerpVel.y, this.vel.y, this.options.lerp),
-    //         }
-
-    //         this.points = this.getPoints();
-
-    //         this.maskPath = this.getMaskPath();
-    //         gsap.to(this.mask, {
-    //             attr: { d: this.maskPath }
-    //         });
-
-    //         const distance = Math.sqrt(Math.pow(this.vel.x, 2) + Math.pow(this.vel.y, 2));
-    //         const scale = Math.min(distance * this.options.delta, 1);
-    //         const angle = (this.vel.x * this.options.delta * 180) / Math.PI;
-    //         gsap.to(this.img, {
-    //             scale: 1 - scale,
-    //             rotate: angle,
-    //         })
-
-    //         this.lastX = this.x;
-    //         this.lastY = this.y;
-    //     }
-    //     init() {
-    //         this.el.parentElement.addEventListener('mousemove', (e) => {
-    //             if (this.paused === true) return;
-    //             this.x = e.clientX;
-    //             this.y = e.clientY;
-    //             this.move();
-    //         });
-    //         this.el.addEventListener('mouseenter', () => {
-    //             if (this.paused === true) return;
-    //             this.toggleVisibility(this.img, true)
-    //         });
-    //         this.el.addEventListener('mouseleave', () => {
-    //             if (this.paused === true) return;
-    //             this.toggleVisibility(this.img, false)
-    //         });
-    //     }
-    //     createHoverImage() {
-    //         let imageElm = new Image(900);
-    //         imageElm.src = this.imgUrl;
-    //         imageElm.classList.add('hover-image');
-    //         // let the browser rasterize the image and hide it after
-    //         // cause strange behavior where browser dont really load images with opacity set to 0
-    //         imageElm.addEventListener('load', () => {
-    //             imageElm.style.opacity = '0.01';
-    //             this.el.appendChild(imageElm);
-    //             setTimeout(() => {
-    //                 this.toggleVisibility(imageElm, false, 0);
-    //             }, 100);
-    //         });
-    //         return imageElm;
-    //     }
-    //     move() {
-    //         this.imgDimensions = this.getDimensions(this.img)
-    //         const y = this.y - this.imgDimensions.height / 2;
-    //         const x = this.x - this.imgDimensions.width / 2;
-    //         gsap.to(this.img, {
-    //             y: y,
-    //             x: x,
-    //         });
-    //     }
-    //     createMask() {
-    //         let maskpath = document.querySelector('#hover-image__mask path');
-    //         this.img.style.cssText +=
-    //             '-webkit-clip-path: url(#hover-image__mask);clip-path: url(#hover-image__mask);';
-    //         if (maskpath) return maskpath;
-
-    //         document.body.insertAdjacentHTML(
-    //             'beforeend',
-    //             `
-    //                     <svg height="0" width="0" style="position:absolute;">
-    //                         <!--   https://yqnn.github.io/svg-path-editor/ -->
-    //                         <defs>
-    //                             <clipPath id="hover-image__mask" clipPathUnits="objectBoundingBox">
-    //                             <path 
-    //                                 d="${this.maskPath}"
-    //                                 data-path-normal="${this.maskPath}"
-    //                             />
-    //                             </clipPath>
-    //                         </defs>
-    //                     </svg>
-    //                     `
-    //         );
-    //         return document.querySelector('#hover-image__mask path');
-    //     }
-    //     toggleVisibility(el, show, duration = null) {
-    //         let time = {};
-    //         if (duration !== null) {
-    //             time = {
-    //                 duration: 0,
-    //             };
-    //         }
-    //         gsap.to(el, {
-    //             opacity: show ? 1 : 0,
-    //             ...time,
-    //         });
-    //     }
-    //     getMaskPath() {
-    //         return `M ${this.options.base} ${this.options.base} C ${this.points.left.top} 0.25 ${this.points.left.bottom} 0.75 ${this.options.base} ${1 - this.options.base} C 0.25 ${this.points.bottom.left} 0.75 ${this.points.bottom.right} ${1 - this.options.base} ${1 - this.options.base} C ${this.points.right.bottom} 0.75 ${this.points.right.top} 0.25 ${1 - this.options.base} ${this.options.base} C 0.75 ${this.points.top.right} 0.25 ${this.points.top.left} ${this.options.base} ${this.options.base} Z`;
-    //     }
-    //     getPoints() {
-    //         return {
-    //             left: {
-    //                 top: this.options.base + (this.options.base / 100 * this.lerpVel.x),
-    //                 bottom: this.options.base + (this.options.base / 100 * this.lerpVel.x),
-    //             },
-    //             bottom: {
-    //                 left: (1 - this.options.base) + (this.options.base / 100 * this.lerpVel.y),
-    //                 right: (1 - this.options.base) + (this.options.base / 100 * this.lerpVel.y),
-    //             },
-    //             right: {
-    //                 bottom: (1 - this.options.base) + (this.options.base / 100 * this.lerpVel.x),
-    //                 top: (1 - this.options.base) + (this.options.base / 100 * this.lerpVel.x),
-    //             },
-    //             top: {
-    //                 right: this.options.base + (this.options.base / 100 * this.lerpVel.y),
-    //                 left: this.options.base + (this.options.base / 100 * this.lerpVel.y),
-    //             },
-    //         };
-    //     }
-    //     getDimensions(el) {
-    //         return { width: el.clientWidth, height: el.clientHeight };
-    //     }
-    //     start() {
-    //         this.paused = false;
-    //         this.render();
-    //     }
-    //     stop() {
-    //         this.paused = true;
-    //     }
-    // }
-    // function clamp(val, min, max) {
-    //     return Math.min(Math.max(val, min), max);
-    // }
-    // function lerp(v0, v1, t) {
-    //     return v0 * (1 - t) + v1 * t;
-    // }
-
-    // for (const item of document.querySelectorAll("[data-hover-img]")) {
-    //     new HoverImage(item);
-    // }
-})
+// for (const item of document.querySelectorAll("[data-hover-img]")) {
+//     new HoverImage(item);
+// }
+// })
